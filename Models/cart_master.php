53d5e9ee9ce480f6f617e4568084c5189981f204
Rahul <87427144+Rahul-R-M@users.noreply.github.com>
2022-11-15 23:22:38 +0530

    Add files via upload

---
+++ Models/cart_master.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class cart_master extends Model
+{
+    use HasFactory;
+    public $table="cart_master";
+    public $timestamps=false;
+}
